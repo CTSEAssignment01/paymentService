a783d783730abb603781af49b8e580e79914b8a0
Widanagama K.L <kusalyawidanagama@gmail.com>
2026-05-02 15:47:09 +0530

    Update PaymentTransactionRepository.java

--- src/main/java/com/example/paymentservice/repository/PaymentTransactionRepository.java
+++ src/main/java/com/example/paymentservice/repository/PaymentTransactionRepository.java
@@ -2,16 +2,14 @@ package com.example.paymentservice.repository;
 
 import com.example.paymentservice.model.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.List;
-
 
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, UUID> {
 
     Optional<PaymentTransaction> findByStripeSessionId(String stripeSessionId);
 
-    List<PaymentTransaction> findFinalTransactionsByUserIdOrderByCreatedAtDesc(UUID userId, String status);
+    List<PaymentTransaction> findByUserIdAndStatusOrderByCreatedAtDesc(UUID userId, String status);
 }
